e564a471a99188ad2476d5a9be159766df29a828
CircleCI <circleci@spacet.me>
2018-04-20 19:04:09 +0000

    Deploy df343a7cc1ede170c0ced2d5808b1325e7321bec to GitHub pages [ci skip]

--- static.4f9705d2.js
+++ static.4f9705d2.js
@@ -67,7 +67,7 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "https://reactbkk.com/";
+/******/ 	__webpack_require__.p = "https://reactbkk.com/3.0.0/";
 /******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(__webpack_require__.s = 8);
@@ -1483,4 +1483,4 @@ module.exports = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACq
 /***/ })
 /******/ ]);
 });
-//# sourceMappingURL=static.c71d5e36.js.map
+//# sourceMappingURL=static.4f9705d2.js.map
