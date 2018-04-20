44e62ae581a9f5032559dab38312464206ab00db
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2018-04-21 01:58:28 +0700

    :sparkles: Add favicon
    Co-authored-by: Phoomparin <phoomparin@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Router, Link } from 'react-static'
+import { Router, Head } from 'react-static'
 import { css } from 'glamor'
 import glamorous from 'glamorous'
 import { hot } from 'react-hot-loader'
-//
 import Routes from 'react-static-routes'
+import favicon from './favicon.png'
 
 css.global('html, body', tw('font-sans font-light text-base text-white m-0 p-0 bg-black'))
 css.global('a', tw('no-underline text-react-blue'))
@@ -14,6 +14,10 @@ const Content = glamorous.div(tw('p-4'))
 const App = () => (
   <Router>
     <div>
+      <Head>
+        <title>React Bangkok 3.0.0</title>
+        <link rel="shortcut icon" type="image/png" href={favicon} />
+      </Head>
       <Content>
         <Routes />
       </Content>
