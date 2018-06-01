98e6178f1fbd59d496eeb73f538f982ee81d5cd2
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2018-06-02 00:53:20 +0700

    :wrench: Use _redirects file instead of <meta> redirect

--- .netlify/build.sh
+++ .netlify/build.sh
@@ -3,6 +3,6 @@ yarn build
 rm -rf .netlify/dist
 mkdir -p .netlify/dist
 cp -r dist .netlify/dist/3.0.0
-cat > .netlify/dist/index.html <<'EOF'
-<meta http-equiv="refresh" content="0;url=3.0.0">
+cat > .netlify/dist/__redirects <<'EOF'
+/ /3.0.0/
 EOF
