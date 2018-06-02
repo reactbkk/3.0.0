9b8ad7bc8623a8140b4151331a39036a20549027
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2018-06-03 03:09:55 +0700

    :sparkles: Implement admin tool for giving access code

--- src/redeem/RedeemDialog.js
+++ src/redeem/RedeemDialog.js
@@ -2,6 +2,7 @@ import { Dialog, Button, Preloader } from 'reactackle'
 import React from 'react'
 import GitHubIcon from 'react-icons/lib/fa/github'
 import ArrowForward from 'react-icons/lib/fa/angle-right'
+import axios from 'axios'
 
 import { firebase } from '../firebase'
 
@@ -26,40 +27,7 @@ class RedeemDialogContent extends React.Component {
     status: 'initial',
     error: '',
     code: '',
-  }
-
-  signIn = async () => {
-    this.setState({ status: 'signingIn' })
-    try {
-      let result
-      try {
-        result = await firebase.auth().signInWithPopup(new firebase.auth.GithubAuthProvider())
-      } catch (e) {
-        this.setState({ status: 'error', error: `Cannot sign in: ${e}` })
-        return
-      }
-      const uid = result.user.providerData[0].uid
-      let document
-      this.setState({ status: 'checking' })
-      try {
-        document = await firebase
-          .firestore()
-          .collection('github_users')
-          .doc(uid)
-          .get()
-      } catch (e) {
-        this.setState({ status: 'error', error: `Cannot check: ${e}` })
-        return
-      }
-      const data = document.data()
-      this.setState({ status: 'checked', code: data && data.code })
-    } catch (e) {
-      this.setState({ status: 'error', error: `Unknown error: ${e}` })
-    }
-  }
-
-  openEventPop = () => {
-    window.open('https://www.eventpop.me/e/3607-react-bangkok-3-0-0', '_blank')
+    admin: false,
   }
 
   render () {
@@ -86,10 +54,19 @@ class RedeemDialogContent extends React.Component {
       )
     }
     if (this.state.status === 'checked') {
+      const adminPanel = this.state.admin && (
+        <div>
+          <p>
+            Hey you are an admin! You can{' '}
+            <Button onPress={this.giveCode} text="give redemption codes!" />
+          </p>
+        </div>
+      )
       if (!this.state.code) {
         return (
           <div>
             <p>Sorry, we cannot find the ticket associated with your account...</p>
+            {adminPanel}
           </div>
         )
       }
@@ -107,6 +84,7 @@ class RedeemDialogContent extends React.Component {
             icon={<ArrowForward />}
             onPress={this.openEventPop}
           />
+          {adminPanel}
         </div>
       )
     }
@@ -122,5 +100,70 @@ class RedeemDialogContent extends React.Component {
         />
       </div>
     )
+  }
+
+  signIn = async () => {
+    this.setState({ status: 'signingIn' })
+    try {
+      let result
+      try {
+        result = await firebase.auth().signInWithPopup(new firebase.auth.GithubAuthProvider())
+      } catch (e) {
+        this.setState({ status: 'error', error: `Cannot sign in: ${e}` })
+        return
+      }
+      const uid = result.user.providerData[0].uid
+      let document
+      this.setState({ status: 'checking' })
+      try {
+        document = await firebase
+          .firestore()
+          .collection('github_users')
+          .doc(uid)
+          .get()
+      } catch (e) {
+        this.setState({ status: 'error', error: `Cannot check: ${e}` })
+        return
+      }
+      const data = document.data()
+      this.setState({ status: 'checked', code: data && data.code, admin: data && data.admin })
+    } catch (e) {
+      this.setState({ status: 'error', error: `Unknown error: ${e}` })
+    }
+  }
+
+  openEventPop = () => {
+    window.open('https://www.eventpop.me/e/3607-react-bangkok-3-0-0', '_blank')
+  }
+
+  giveCode = async () => {
+    const username = window.prompt('Enter GitHub username')
+    if (!username) {
+      window.alert('Aborting.')
+      return
+    }
+    try {
+      const data = (await axios.get(`https://api.github.com/users/${username}`)).data
+      if (!data) throw new Error('No response from GitHub')
+      if (!data.id) throw new Error('No user ID data found')
+      const id = data.id
+      window.alert(`Found GitHub user id = ${id}.`)
+      const redemptionCode = window.prompt('Enter redemption code')
+      if (!redemptionCode) {
+        window.alert('Aborting.')
+        return
+      }
+      await firebase
+        .firestore()
+        .collection('github_users')
+        .doc(String(id))
+        .update({
+          code: redemptionCode,
+          username,
+        })
+      window.alert(`OK done.\n\nusername=${username}\nid=${id}\ncode=${redemptionCode}`)
+    } catch (e) {
+      window.alert(`Cannot giveCode: ${e}.`)
+    }
   }
 }
