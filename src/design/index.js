216c85d500d50002f12a67a63dca0b4512115c2b
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2018-06-04 00:59:49 +0700

    :lipstick: Add font-face for semibold Noto Sans, Noto Sans Thai UI

--- src/design/index.js
+++ src/design/index.js
@@ -140,7 +140,9 @@ function injectGlobalStyles () {
     ${fontFace(require('./vendor/fonts/Metropolis-SemiBold.woff2'), 'Metropolis', 600)}
     ${fontFace(require('./vendor/fonts/Metropolis-Bold.woff2'), 'Metropolis', 700)}
     ${fontFace(require('./vendor/fonts/NotoSans-Light-Latin.woff2'), 'Noto Sans', 300)}
+    ${fontFace(require('./vendor/fonts/NotoSans-SemiBold-Latin.woff2'), 'Noto Sans', 600)}
     ${fontFace(require('./vendor/fonts/NotoSansThaiUI-Light.woff2'), 'Noto Sans Thai UI', 300)}
+    ${fontFace(require('./vendor/fonts/NotoSansThaiUI-SemiBold.woff2'), 'Noto Sans Thai UI', 600)}
   `
 
   injectGlobal({
