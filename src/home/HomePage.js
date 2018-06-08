c0231391b1da8999a6974725f9fe9ef436c18c93
Thai Pangsakulyanont <dtinth@spacet.me>
2018-06-08 12:06:39 +0700

    Update HomePage.js

--- src/home/HomePage.js
+++ src/home/HomePage.js
@@ -113,7 +113,7 @@ function TicketsSection () {
             Details will be available soon!
           </p>
           <p>
-            <ActionButton href="https://www.facebook.com/reactbkk">Information</ActionButton>
+            <ActionButton href="https://www.facebook.com/reactbkk/photos/a.161749477831615.1073741828.161742341165662/172068550133041/?type=3">Information</ActionButton>
           </p>
           <p>{renderRedeemTicketButton()}</p>
         </TypographicContext>
